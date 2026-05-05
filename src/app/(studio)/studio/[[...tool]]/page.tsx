24693def53f989d5ada91a6d3eed6b284c8c70f2
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2026-05-05 10:08:45 +0530

    refactor: Make NextStudio a dynamic import

--- src/app/(studio)/studio/[[...tool]]/page.tsx
+++ src/app/(studio)/studio/[[...tool]]/page.tsx
@@ -7,14 +7,12 @@
  * https://github.com/sanity-io/next-sanity
  */
 
-import { NextStudio } from "next-sanity/studio"
-
-import config from "../../../../../sanity.config"
+import NextStudioWrapper from "./NextStudioWrapper"
 
 export const dynamic = "force-static"
 
 export { metadata, viewport } from "next-sanity/studio"
 
 export default function StudioPage() {
-  return <NextStudio config={config} />
+  return <NextStudioWrapper />
 }
